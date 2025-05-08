af738b29e8aa99dd1e07619644dcd3494e795c16
MoWafy001 <wafy123445@gmail.com>
2025-05-08 20:04:32 +0300

    feat: update seed_all command to seed 15 customers and create unassigned tickets for each admin

--- tickets/management/commands/seed_all.py
+++ tickets/management/commands/seed_all.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from users.models import User, Admin, SupportAgent
 from customers.models import Customer
+from tickets.models import Ticket
 
 class Command(BaseCommand):
     help = "Seed the database with mock data for all models"
@@ -30,13 +31,25 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS(f"Support Agent {support_agent.username} created."))
         self.stdout.write(self.style.SUCCESS("5 Support Agents created."))
 
-        # Seed 5 customers
-        for _ in range(1,6):
+        # Seed 15 customers
+        for _ in range(1,16):
             customer = Customer.objects.create_customer(
                 name=f"Customer {_}",
                 email=f"custtomer-{_}@app.com",
             )
             self.stdout.write(self.style.SUCCESS(f"Customer {customer.name} created."))
-        self.stdout.write(self.style.SUCCESS("5 Customers created."))
+        self.stdout.write(self.style.SUCCESS("15 Customers created."))
+
+        # create 30 unassigned tickets for each admin
+        for admin in Admin.objects.all():
+            for _ in range(1,31):
+                ticket = Ticket.objects.create(
+                    title=f"Ticket {_} created by {admin.username}",
+                    description=f"Description for ticket {_} created by {admin.username}",
+                    created_by=admin,
+                )
+                self.stdout.write(self.style.SUCCESS(f"Ticket {ticket.title} created."))
+            self.stdout.write(self.style.SUCCESS(f"30 unassigned tickets created by {admin.username}."))
+
 
         self.stdout.write(self.style.SUCCESS("Database seeded successfully!"))
