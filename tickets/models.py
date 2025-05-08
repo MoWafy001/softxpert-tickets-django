0bbff7a56830b101940fcc317b9f7827b8882a90
MoWafy001 <wafy123445@gmail.com>
2025-05-08 20:02:03 +0300

    feat: add Ticket model with fields and initial migration

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    assigned_to = models.ForeignKey(
+        'users.SupportAgent',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='tickets_assigned'
+    )
+    created_by = models.ForeignKey(
+        'users.Admin',
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='tickets_created'
+    )
+    sold_to = models.ForeignKey(
+        'customers.Customer',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='tickets_sold'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
