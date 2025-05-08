97095771a346247f257a4112fec5e5996428feb2
MoWafy001 <wafy123445@gmail.com>
2025-05-08 05:42:44 +0300

    feat: configure Django REST framework authentication and permission settings

--- support_system/settings.py
+++ support_system/settings.py
@@ -83,6 +83,17 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'UNAUTHENTICATED_USER': None,
+    'UNAUTHENTICATED_TOKEN': None,
+}
+
 WSGI_APPLICATION = 'support_system.wsgi.application'
 
 
