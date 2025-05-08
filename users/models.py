8a1c5e80644071d58bbeb17f6eeb85b7526d4d4d
MoWafy001 <wafy123445@gmail.com>
2025-05-08 03:59:55 +0300

    feat: update requirements and add Django REST framework support

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from .enums import Role
 
 # Create your models here.
 class User(models.Model):
-    __tablename__ = "users"
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128)
@@ -14,3 +13,6 @@ class User(models.Model):
     ])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
